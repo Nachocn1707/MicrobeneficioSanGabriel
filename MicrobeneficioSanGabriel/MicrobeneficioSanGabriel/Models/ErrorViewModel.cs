095bf33cd4c48f004081ca64c9fd69339cbba4e1
Nachocn1707 <icascante70274@ufide.ac.cr>
2026-05-11 19:41:10 -0600

    chore: reorganizacion inicial del proyecto FastAPI Vue PostgreSQL

--- MicrobeneficioSanGabriel/MicrobeneficioSanGabriel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MicrobeneficioSanGabriel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
